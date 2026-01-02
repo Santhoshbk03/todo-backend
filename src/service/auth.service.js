b4489de47bc9571c6167e184beacbd7e6580146f
Santhosh Babu <santhosh.b@maxbytetech.com>
2026-01-02 13:04:11 +0530

    auth service updated

--- src/service/auth.service.js
+++ src/service/auth.service.js
@@ -2,15 +2,15 @@ import pool from "../config/connect.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
-
 class AuthService {
   registerService = async ({ name, email, password }) => {
     if (!name || !email || !password) {
       throw { status: 400, message: "All fields are required" };
     }
-    const existingUser = await pool.query("SELECT id FROM users WHERE email=$1", [
-      email,
-    ]);
+    const existingUser = await pool.query(
+      "SELECT id FROM users WHERE email=$1",
+      [email]
+    );
     if (existingUser.rows.length > 0) {
       throw { status: 409, message: "Email already exists" };
     }
@@ -47,11 +47,15 @@ class AuthService {
       throw { status: 401, message: "Invalid email or password" };
     }
 
-    const token = jwt.sign({ id: user.id, email: user.email },process.env.JWT_SECRET_KEY ,{ expiresIn: process.env.JWT_EXPIRES_IN } )
+    const token = jwt.sign(
+      { id: user.id, email: user.email },
+      process.env.JWT_SECRET,
+      { expiresIn: process.env.JWT_EXPIRES_IN || "1d" }
+    );
 
-    const { password: _, ...data  } = user;
-    return {data,token};
+    const { password: _, ...data } = user;
+    return { data, token };
   };
 }
 
-export default AuthService; 
+export default AuthService;
